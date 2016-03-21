45fdb7a2679abff30ab64b9929ecd0bf92ef3f4c
Amandeep Chawla <amandeep@adobe.com>
2016-03-21 15:43:03 +0530

    Using Express Router

--- Coursera - Server Side Developement with NodeJS/Exercises/003node-express/server.js
+++ Coursera - Server Side Developement with NodeJS/Exercises/003node-express/server.js
@@ -8,37 +8,42 @@ var port = 3000;
 var app = express();
 
 app.use(morgan('dev'));
-app.use(bodyParser.json());
 
-app.all('/dishes', function(req,res,next) {
-	res.writeHead(200, {'Content-Type':"text/plain"});
-	next();
-});
+var dishRouter = express.Router();
+dishRouter.use(bodyParser.json());
 
-app.get('/dishes', function(req,res,next){
-	res.end('Will send all the dishes to you!');
-});
+dishRouter.route('/')
+	.all(function(req,res,next) {
+		res.writeHead(200, {'Content-Type':"text/plain"});
+		next();
+	})
+	.get(function(req,res,next){
+		res.end('Will send all the dishes to you!');
+	})
+	.post(function(req,res,next){
+		res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
+	})
+	.delete(function(req,res,next){
+		res.end('Deleting all dishes');
+	});
 
-app.post('/dishes', function(req,res,next){
-	res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
-});
+dishRouter.route('/:dishId')
+	.all(function(req,res,next) {
+		res.writeHead(200, {'Content-Type':"text/plain"});
+		next();
+	})
+	.get(function(req,res,next){
+		res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
+	})
+	.put(function(req,res,next){
+		res.write('Updating the dish: ' + req.params.dishId + ' \n');
+		res.end('Will update details of the dish: ' + req.body.name + ' with details: ' + req.body.description);
+	})
+	.delete( function(req,res,next){
+		res.end('Deleting dish: ' + req.params.dishId);
+	});
 
-app.delete('/dishes', function(req,res,next){
-	res.end('Deleting all dishes');
-});
-
-app.get('/dishes/:dishId', function(req,res,next){
-	res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
-});
-
-app.put('/dishes/:dishId', function(req,res,next){
-	res.write('Updating the dish: ' + req.params.dishId + ' \n');
-	res.end('Will update details of the dish: ' + req.body.name + ' with details: ' + req.body.description);
-});
-
-app.delete('/dishes/:dishId', function(req,res,next){
-	res.end('Deleting dish: ' + req.params.dishId);
-});
+app.use('/dishes', dishRouter);
 
 app.use(express.static(__dirname + '/public'));
 
