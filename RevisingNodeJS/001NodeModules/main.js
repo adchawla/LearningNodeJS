761925d46afcaeb71331c04fb7d0a7ca13140965
Amandeep Chawla <adchawla@gmail.com>
2017-05-15 15:28:53 +0530

    Defining and Using Node Modules

---
+++ RevisingNodeJS/001NodeModules/main.js
@@ -0,0 +1,5 @@
+var rect = require('./rectangle')();
+var square = require('./square');
+
+console.log("Perimeter of rectangle is : " + rect.perimeter(5, 7));
+console.log("Area of square is : " + square.perimeter(4));
---
+++ RevisingNodeJS/001NodeModules/square.js
@@ -0,0 +1,7 @@
+exports.perimeter = function(x) {
+    return 4 * x;
+}
+
+exports.area = function(x) {
+    return ( x * x);
+}
