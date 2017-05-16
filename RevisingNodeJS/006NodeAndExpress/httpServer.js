9ca2add233a83340a0ee422208e0469452dcc3af
Amandeep Chawla <adchawla@gmail.com>
2017-05-16 23:28:31 +0530

    Using routers to make code much cleaner

--- RevisingNodeJS/006NodeAndExpress/httpServer.js
+++ RevisingNodeJS/006NodeAndExpress/httpServer.js
@@ -8,33 +8,37 @@ var port = 3000;
 var app = express();
 
 app.use(morgan('dev'));
-app.use(bodyParser.json());
 
-app.all('/dishes', function(req, res, next){
+var dishRouter = express.Router();
+dishRouter.use(bodyParser.json());
+
+dishRouter.route('/')
+.all(function(req, res, next){
     res.writeHead(200, {'Content-Type': 'text/plain'});
     next();
-});
-app.get('/dishes', function(req, res, next){
+})
+.get(function(req, res, next){
     res.end('Will send all the dishes to you!');
-});
-app.post('/dishes', function(req, res, next){
+})
+.post(function(req, res, next){
     res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
-});
-app.delete('/dishes', function(req, res, next) {
+})
+.delete(function(req, res, next) {
     res.end('All the dishes will be deleted');
 });
 
-app.get('/dishes/:dishId', function(req, res, next) {
+dishRouter.route('/:dishId')
+.get(function(req, res, next) {
     res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
-});
-
-app.put('/dishes/:dishId', function(req, res, next) {
+})
+.put(function(req, res, next) {
     res.end('Will update the details of the dish: ' + req.params.dishId + ' for ' + req.body.name + ' with details: ' + req.body.description);
-});
-
-app.delete('/dishes/:dishId', function(req, res, next) {
+})
+.delete(function(req, res, next) {
     res.end('Will delete the dish: ' + req.params.dishId);
 });
+
+app.use('/dishes', dishRouter);
 
 app.use(express.static(__dirname + '/public'));
 
