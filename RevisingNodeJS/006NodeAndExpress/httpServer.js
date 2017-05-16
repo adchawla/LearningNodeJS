0c22c2d229cf38eea4c3020d83574ba24940c087
Amandeep Chawla <adchawla@gmail.com>
2017-05-16 19:03:23 +0530

    Using express static to serve the static files

--- RevisingNodeJS/006NodeAndExpress/httpServer.js
+++ RevisingNodeJS/006NodeAndExpress/httpServer.js
@@ -1,17 +1,16 @@
 var http = require('http');
 var express = require('express');
+var morgan = require('morgan');
 
 var hostname = 'localhost';
 var port = 3000;
 
 var app = express();
 
-app.use(function(req, res, next){
-    res.writeHead(200, {'ContentType':'text/html'});
-    res.end('<html><body><h1>Hello World!</h1></body></html>');
-});
+app.use(morgan('dev'));
 
-var server = http.createServer(app);
-server.listen(port, hostname, function() {
-    console.log(`Server running at http://${hostname}:{port}`);
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, hostname, function() {
+    console.log(`Server running at http://${hostname}:${port}`);    
 });
