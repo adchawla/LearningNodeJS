683f9b40febb4b4f3f622a4bf1610d73a7c9c691
Amandeep Chawla <adchawla@gmail.com>
2017-05-16 17:57:19 +0530

    Serving static html pages using http module

--- RevisingNodeJS/005NodeAndHttp/httpServer.js
+++ RevisingNodeJS/005NodeAndHttp/httpServer.js
@@ -1,13 +1,37 @@
 var http = require('http');
+var fs = require('fs');
+var path = require('path');
 
 var hostname = 'localhost';
 var port = 3000;
 
 var server = http.createServer(function(req, res) {
-    console.log(req.headers);
-
-   res.writeHead(200, {'ContentType':'text/html'});
-   res.end('<h1>Hello World</h1>') 
+    if (req.method == 'GET') {
+        var fileurl;
+        if (req.url == '/') {
+            fileurl = '/index.html';
+        } else {
+            fileurl = req.url;
+        }
+        var filePath = path.resolve('./public'+fileurl);
+        var fileExt = path.extname(filePath);
+        if (fileExt == '.html' || fileExt == '.htm') {
+            fs.exists(filePath, function(exists) {
+                if (!exists) {
+                    res.writeHead(404, {'ContentType':'text/html'});
+                    res.end('<h1>Error 404: ' + fileurl + ' not found </h1>');
+                } else {
+                    res.writeHead(200, {'ContentType':'text/html'});
+                    fs.createReadStream(filePath).pipe(res);
+                }
+            });
+        } else {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.end('<html><body><h1>Error 404: ' + fileurl + ' not a HTML file</h1></body></html>');
+        }
+    }
 });
 
-server.listen(port, hostname);
+server.listen(port, hostname, function() {
+    console.log('Server running at http://${hostname}:{port}');
+});
